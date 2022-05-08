81f2eb9ed0132d930c98ed94c9ad62ac6d9252a2
Lucas Salazar <lucassalazar741@gmail.com>
2022-05-07 22:55:26 -0300

    Implements the function get_cheapest_hotel()

--- src/my_module.py
+++ src/my_module.py
@@ -1,3 +1,28 @@
+from ast import If
+from .helpers import parse_client_data
+from .models import Hotel, Client
+
+
 def get_cheapest_hotel(number):   #DO NOT change the function's name
-    cheapest_hotel = "cheapest_hotel_name"
-    return cheapest_hotel
+    """
+    Gets the name of the cheapest hotel given some dates.
+    number: string of dates and client type
+    returns: the string name of a hotel
+    """
+    data = parse_client_data(number)
+    client = Client(type=data["type"], stay_dates=data["stay_dates"])
+    hotels = Hotel.Objects.all()
+    cheapest_hotel = None
+    
+    for hotel in hotels:
+        if not cheapest_hotel:
+            cheapest_hotel = hotel
+
+        costs = hotel.get_costs(client)
+        classification = hotel.classification
+
+        if costs <= cheapest_hotel.get_costs(client) and classification > cheapest_hotel.classification:
+            cheapest_hotel = hotel
+
+
+    return cheapest_hotel.name
